23a85e72ac284eaef119fa60dad868dc972c1712
zhangz128 <zhangzz1218@gmail.com>
2023-12-12 11:26:32 -0800

    user exception

--- Banking/user/transaction.py
+++ Banking/user/transaction.py
@@ -1,102 +1,97 @@
 import time
 
 
+class AccountError(Exception):
+    """Custom exception for account-related errors."""
+    pass
+
 class transaction:
     def __init__(self, allUser):
         self.allUser = allUser
 
-    def balance(self):
-        cardNum = input("Please enter your account number: ")
-        user = self.allUser.get(cardNum)
-        if not user: 
-            print("This account is not exited.")
-            return  -1
-        if user.card.cardLock:
-            print("This account is locked down.")
-            return -1
-        if not self.checkPasswd(user.card.cardPasswd):
-            print("Wrong password. Your account is locked down...")
-            user.card.cardLock = True
-            return -1
-        print("Account number：%s  Account balance：%d" % (user.card.cardId, user.card.cardMoney))
-        
-    def withdraw(self):
-        cardNum = input("Please enter your account number: ")
+    def _validate_account(self, cardNum):
         user = self.allUser.get(cardNum)
         if not user:
-            print("This account is not exited.")
-            return -1
+            raise AccountError("This account does not exist.")
         if user.card.cardLock:
-            print("This account is locked down.")
-            return -1
-        if not self.checkPasswd(user.card.cardPasswd):
-            print("Wrong password. Your account is locked down...")
-            user.card.cardLock = True
-            return -1
+            raise AccountError("This account is locked down.")
+        return user
 
-        money = int(input("Please enter withdraw amount："))
-        if money > user.card.cardMoney:
-            print("Exceed account balance. Fail for withdraw..")
-            return -1
-        if money < 0:
-            print("No avaliable account balance. Fail for withdraw...")
-            return -1
-        user.card.cardMoney -= money
-        print("Withdraw success. Account banlance: %d" % (user.card.cardMoney))
+    def _check_password(self, realPasswd):
+        for i in range(3):
+            tempPasswd = input("Please enter your password: ")
+            if tempPasswd == realPasswd:
+                return True
+        raise AccountError("Wrong password. Your account is locked down...")
+
+    def balance(self):
+        try:
+            cardNum = input("Please enter your account number: ")
+            user = self._validate_account(cardNum)
+            if not self._check_password(user.card.cardPasswd):
+                user.card.cardLock = True
+                return
+            print("Account number：%s  Account balance：%d" % (user.card.cardId, user.card.cardMoney))
+        except AccountError as e:
+            print(e)
+
+    def withdraw(self):
+        try:
+            cardNum = input("Please enter your account number: ")
+            user = self._validate_account(cardNum)
+            self._check_password(user.card.cardPasswd)
+            
+            money = int(input("Please enter withdraw amount："))
+            if money > user.card.cardMoney or money < 0:
+                raise AccountError("Invalid amount. Withdrawal failed.")
+
+            user.card.cardMoney -= money
+            print("Withdraw success. Account balance: %d" % user.card.cardMoney)
+        except AccountError as e:
+            print(e)
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
 
     def deposit(self):
-        cardNum = input("Please enter your account number: ")
-        user = self.allUser.get(cardNum)
-        if not user:
-            print("This account is not exited.")
-            return -1
-        if user.card.cardLock:
-            print("This account is locked down.")
-            return -1
-        if not self.checkPasswd(user.card.cardPasswd):
-            print("Wrong password. Your account is locked down...")
-            user.card.cardLock = True
-            return -1
+        try:
+            cardNum = input("Please enter your account number: ")
+            user = self._validate_account(cardNum)
+            self._check_password(user.card.cardPasswd)
+            
+            money = int(input("Please enter deposit amount："))
+            if money < 0:
+                raise AccountError("Invalid amount. Deposit failed.")
 
-        money = int(input("Please enter deposit amount："))
-        if money < 0:
-            print("Invalid amount. Fail for deposit...")
-            return -1
-        user.card.cardMoney += money
-        print("Deposit success: %d Account balance: %d" % (money, user.card.cardMoney))
+            user.card.cardMoney += money
+            print("Deposit success: %d. Account balance: %d" % (money, user.card.cardMoney))
+        except AccountError as e:
+            print(e)
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
 
     def transfer(self):
-        cardNum = input("Please enter your account number: ")
-        user = self.allUser.get(cardNum)
-        if not user: 
-            print("This account is not exited.")
-            return -1
-        if user.card.cardLock:
-            print("This account is locked down.")
-            return -1
-        if not self.checkPasswd(user.card.cardPasswd):
-            print("Wrong password. Your account is locked down...")
-            user.card.cardLock = True
-            return -1
+        try:
+            cardNum = input("Please enter your account number: ")
+            user = self._validate_account(cardNum)
+            self._check_password(user.card.cardPasswd)
 
-        money = int(input("Please enter transfer amount："))
-        if money > user.card.cardMoney or money < 0:
-            print("Invalid amount. Fail for transfer...")
-            return -1
-        newcard = input("Please enter the receive account number：")
-        newcard = self.allUser.get(newcard)
-        if not newcard:
-            print("This account is not exited. Fail for transfer...")
-            return -1
-        if newcard.card.cardLock:
-            print("This receive account is locked. Fail for transfer...")
-            return -1
-        user.card.cardMoney -= money
-        newcard.card.cardMoney += money
-        time.sleep(1)
-        print("Transfer success")
-        time.sleep(1)
-        print("Transfer amount: %d Account balance %d" % (money, user.card.cardMoney))
+            money = int(input("Please enter transfer amount："))
+            if money > user.card.cardMoney or money < 0:
+                raise AccountError("Invalid amount. Transfer failed.")
+
+            newcardNum = input("Please enter the recipient account number：")
+            newcard = self._validate_account(newcardNum)
+
+            user.card.cardMoney -= money
+            newcard.card.cardMoney += money
+            time.sleep(1)
+            print("Transfer success")
+            time.sleep(1)
+            print("Transfer amount: %d. Account balance: %d" % (money, user.card.cardMoney))
+        except AccountError as e:
+            print(e)
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
 
     def checkPasswd(self, realPasswd):
         for i in range(3):
